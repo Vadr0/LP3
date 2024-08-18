6dff11b04317e9116a0bc83b420e7890b08b3da6
Yhosfer Anderson <yhosfer.quispe@ucsm.edu.pe>
2024-08-18 16:23:34 -0500

    Add files via upload

---
+++ actividad 1/Ejercicio2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int[] arreglo = new int[10];
+        leerArreglo(arreglo);
+
+        for (int elemento : arreglo) {
+            System.out.printf(elemento+" ");
+        }
+
+    }
+    public static void leerArreglo(int[] arreglo) {
+        Scanner entrada = new Scanner(System.in);
+        for (int i = 0; i < arreglo.length; i++) {
+            if (i == 0){
+                System.out.printf("Ingrese " + (i + 1) + "° valor: ");
+                arreglo[i] = entrada.nextInt();
+            }
+            while (i > 0){
+                System.out.printf("ingrese "+(i+1)+"° valor: ");
+                arreglo[i] = entrada.nextInt();
+                if (arreglo[i] > arreglo[i-1]){
+                    break;
+                }else {
+                    System.out.println("El numero debe ser mayor a "+arreglo[i-1]);
+                }
+            }
+        }
+    }
+}
