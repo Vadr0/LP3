4148a1ce7ac41f064276f48a30ea17d31f6c2d1a
rodel2A <164429797+rodel2A@users.noreply.github.com>
2024-08-17 23:08:19 -0500

    Create ejercicio3.java

---
+++ actividad 1/ejercicio3.java
@@ -0,0 +1,14 @@
+public class MenorNumero {
+    public static void main(String[] args) {
+        double num1 = 5.6;
+        double num2 = 2.4;
+        double num3 = 8.1;
+        
+        double menor = menorDeTres(num1, num2, num3);
+        System.out.println("El menor número es: " + menor);
+    }
+
+    public static double menorDeTres(double a, double b, double c) {
+        return Math.min(a, Math.min(b, c));
+    }
+}
