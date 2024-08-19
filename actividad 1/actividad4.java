36139a4624a4583124d473b1873eadd50886eefd
Yhosfer Anderson <yhosfer.quispe@ucsm.edu.pe>
2024-08-18 20:05:56 -0500

    Corrección

---
+++ actividad 1/actividad4.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class actividad4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.printf("Introduce el tamaño del arreglo: ");
+        int n = scan.nextInt();
+
+        int[] arreglo = new int[n];
+        System.out.println("Introduce los elementos del arreglo: ");
+        leerArreglo(scan, arreglo);
+
+        double promedio = calcularPromedio(arreglo);
+        System.out.printf("El promedio es: "+ promedio);
+
+    }
+
+    public static void leerArreglo(Scanner scanner, int[] arreglo) {
+        for (int i=0; i<arreglo.length; i++) {
+            arreglo[i] = scanner.nextInt();
+        }
+    }
+
+    public static double calcularPromedio(int[] arreglo) {
+        int suma = 0;
+        for (int num : arreglo) {
+            suma += num;
+        }
+        return (double) suma/ arreglo.length;
+    }
+}
