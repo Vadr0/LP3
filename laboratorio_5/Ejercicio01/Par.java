c9d7b5072c0e67dc1d08376aca590d0b0e54d9dc
Vadr0 <164426724+Vadr0@users.noreply.github.com>
2024-10-04 19:06:03 -0500

    Update Par.java

--- laboratorio_5/Ejercicio01/Par.java
+++ laboratorio_5/Ejercicio01/Par.java
@@ -1,24 +1,37 @@
 package Ejercicio01;
 
+// clase generica llamada Par que almacena dos objetos de tipos diferentes
 public class Par <F,S>{
-    private F primero;
-    private S segundo;
+    private F primero; // primer elemento del par
+    private S segundo; // segundo elemento del par
+
+    // constructor para inicializar los valores de primero y segundo
     public Par(F primero, S segundo) {
         this.primero = primero;
         this.segundo = segundo;
     }
+
+    // metodo para obtener el valor del primer elemento
     public F getPrimero() {
         return this.primero;
     }
+
+    // metodo para obtener el valor del segundo elemento
     public S getSegundo() {
         return this.segundo;
     }
+
+    // metodo para actualizar el valor del primer elemento
     public void setPrimero(F primero) {
         this.primero = primero;
     }
+
+    // metodo para actualizar el valor del segundo elemento
     public void setSegundo(S segundo) {
         this.segundo = segundo;
     }
+
+    // metodo para convertir los valores del par a una cadena de texto
     @Override
     public String toString() {
         return "Primero: %s, Segundo: %s".formatted(this.primero, this.segundo);
