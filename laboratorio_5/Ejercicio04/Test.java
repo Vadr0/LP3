8144259d873a9c4f3aa1e55ddf7328393f9480fc
Vadr0 <164426724+Vadr0@users.noreply.github.com>
2024-10-04 19:15:02 -0500

    Update Test.java

--- laboratorio_5/Ejercicio04/Test.java
+++ laboratorio_5/Ejercicio04/Test.java
@@ -1,25 +1,24 @@
 package Ejercicio04;
-
 public class Test {
     public static <F> void main(String[] args) {
-        Par<Double,Integer> par1 = new Par<>(1.2, 1);
-        Par<Double,Integer> par2 = new Par<>(1.3, 1);
-        Par<Double,Integer> par3 = new Par<>(1.4, 1);
-        Par<Double,Integer> par4 = new Par<>(1.5, 1);
+        // crea varios objetos Par con valores Double e Integer
+        Par<Double, Integer> par1 = new Par<>(1.2, 1);
+        Par<Double, Integer> par2 = new Par<>(1.3, 1);
+        Par<Double, Integer> par3 = new Par<>(1.4, 1);
+        Par<Double, Integer> par4 = new Par<>(1.5, 1);
 
-        Contenedor  contePar = new Contenedor();
-        contePar.agregarPar(par1);
-        contePar.agregarPar(par2);
-        contePar.agregarPar(par3);
-        contePar.agregarPar(par4);
-
+        // crea un objeto Contenedor para almacenar los pares
+        Contenedor contePar = new Contenedor();
+        contePar.agregarPar(par1); 
+        contePar.agregarPar(par2); 
+        contePar.agregarPar(par3); 
+        contePar.agregarPar(par4); 
+        // muestra todos los pares almacenados en el contenedor
         contePar.mostrarPares();
-
-
-
     }
-    public static <F,S>void imprimirPar(Par<F,S> par, Par<F,S> par2) {
-        System.out.println("Par 1: "+par.toString());
-        System.out.println("Par 2: "+par2.toString());
+    // metodo para imprimir dos objetos Par
+    public static <F, S> void imprimirPar(Par<F, S> par, Par<F, S> par2) {
+        System.out.println("Par 1: " + par.toString());
+        System.out.println("Par 2: " + par2.toString());
     }
 }
