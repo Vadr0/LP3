16ba037783e84b0dd75c637ce7ccfbf892515d79
Vadr0 <164426724+Vadr0@users.noreply.github.com>
2024-10-04 19:19:30 -0500

    Update Imprimir.java

--- laboratorio_5/Actividad01/Imprimir.java
+++ laboratorio_5/Actividad01/Imprimir.java
@@ -1,32 +1,34 @@
 package Actividad01;
 
+// clase llamada Imprimir que contiene metodos genericos para imprimir arreglos
 public class Imprimir {
-    public static <E> void imprimirArreglo(E [] arregloEntrada){
-        for (E elemento : arregloEntrada){
-            System.out.printf("%s",elemento);
+    // metodo generico para imprimir todos los elementos de un arreglo
+    public static <E> void imprimirArreglo(E[] arregloEntrada) {
+        for (E elemento : arregloEntrada) {
+            System.out.printf("%s", elemento);
         }
-        System.out.println();
+        System.out.println(); 
     }
-    public static <E> void imprimirArreglo(E [] arregloEntrada, int indInfe, int indSuper){
-        if (indInfe < 0 || indSuper >= arregloEntrada.length || indSuper < indInfe || indInfe > indSuper){
-            throw new InvalidSubscripException("Indice fuera de rango");
+    // metodo generico para imprimir los elementos de un arreglo entre dos indices dados
+    public static <E> void imprimirArreglo(E[] arregloEntrada, int indInfe, int indSuper) {
+        // verifica si los indices son validos
+        if (indInfe < 0 || indSuper >= arregloEntrada.length || indSuper < indInfe || indInfe > indSuper) {
+            throw new InvalidSubscripException("Indice fuera de rango"); // lanza excepcion si los indices no son validos
         }
-        for (int i = indInfe; i <= indSuper; i++){
-            System.out.printf("%s \n",arregloEntrada[i]);
-            //System.out.println();
-
+        for (int i = indInfe; i <= indSuper; i++) {
+            System.out.printf("%s \n", arregloEntrada[i]);
         }
-
     }
-
+    // metodo principal para probar los metodos imprimirArreglo
     public static void main(String[] args) {
-        Integer [] arreglosInteger = { 1, 2, 3, 4 , 6, 10};
-        Double [] arreglosDouble = { 1.1, 2.2, 3.3, 4.4, 5.5};
-
-        try{
-            imprimirArreglo(arreglosInteger,0,3);
-        } catch (InvalidSubscripException e){
-            System.out.println("Error: "+e.getMessage());
+        Integer[] arreglosInteger = {1, 2, 3, 4, 6, 10}; // arreglo de enteros
+        Double[] arreglosDouble = {1.1, 2.2, 3.3, 4.4, 5.5}; // arreglo de doubles
+        try {
+            // intenta imprimir una parte del arreglo de enteros
+            imprimirArreglo(arreglosInteger, 0, 3);
+        } catch (InvalidSubscripException e) {
+            // captura la excepcion y muestra el mensaje de error
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
