050769bd4d59b4cbd9cf828e24f37b8a7cec5a13
Yhosfer Anderson Quispe Sullca <yhosfer.quispe@ucsm.edu.pe>
2024-10-13 22:15:59 -0500

    Add files via upload

---
+++ laboratorio_6/Ejercicio_01/Main.java
@@ -0,0 +1,10 @@
+package Ejercicio_01;
+
+public class Main {
+    public static void main(String[] args) {
+        // Se instancia el controlaCarrito
+        ControladorCarrito controlCarrito = new ControladorCarrito();
+        // inicia la interfaz
+        controlCarrito.iniciar();
+    }
+}
