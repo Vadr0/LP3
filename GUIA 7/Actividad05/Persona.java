b6c667a827ce71e305306421945067aa79633f4a
Yhosfer Anderson Quispe Sullca <yhosfer.quispe@ucsm.edu.pe>
2024-10-19 11:12:15 -0500

    Add files via upload

---
+++ GUIA 7/Actividad05/Persona.java
@@ -0,0 +1,19 @@
+package Actividad05;
+
+public class Persona {
+    private String nombre;
+    private String telefono;
+    private String direccion;
+    public Persona(String nombre, String telefono, String direccion) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    @Override
+    public String toString() {
+        return this.nombre + "\t" + this.telefono + "\t" + this.direccion+"\n";
+    }
+}
